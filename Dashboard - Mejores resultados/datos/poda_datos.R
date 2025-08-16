453b775f98908af69eed1aee2e8e2c8a866673bb
Flavius <flaviusciapsa@gmail.com>
2025-08-16 20:20:48 +0200

    Conjunto de datos para el dashboard de mejores resultados

---
+++ Dashboard - Mejores resultados/datos/poda_datos.R
@@ -0,0 +1,27 @@
+# Este script se ha utilizado para descartar los datos redundantes para mi proyecto
+# que se encontraban en el conjunto de datos original obtenido de
+# https://www.kaggle.com/datasets/jeannicolasduval/world-athletics-all-time-rankings
+
+library(readr)
+library(dplyr)
+
+df_original <- read_csv("datos/csvs/world-athletics_all-time-top-lists.csv", 
+                    col_types = cols(all_time_rank = col_integer(), 
+                                     results_score = col_double(),
+                                     event_rank = col_integer(), 
+                                     date_of_birth = col_date(format = "%Y-%m-%d"), 
+                                     pos = col_double(),
+                                     date = col_date(format = "%Y-%m-%d"), 
+                                     year_of_birth = col_integer()))
+
+# Descartar columnas redundantes
+cols_redudantes <- c("all_time_rank", "event_rank", "pos", "wind",
+                           "mark_details", "year_of_birth")
+df_podado <- select(df_original, -all_of(cols_redudantes))
+
+# Descartar observaciones sin valor para results_score
+df_podado <- df_podado[!is.na(df_podado$results_score), ]
+
+#Guardar el nuevo conjunto de datos
+write_csv(df_podado, "datos/csvs/world-athletics_all-time-top-lists_podado.csv",
+          na="", quote="none")
